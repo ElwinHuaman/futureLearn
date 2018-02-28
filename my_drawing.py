5e6767fa5a348dbc65371546320ef1e430204027
Elwin Huaman <elwinlhq@hotmail.com>
2018-02-28 18:48:07 +0100

    Initial commit

---
+++ my_drawing.py
@@ -0,0 +1,26 @@
+from shapes import Triangle, Rectangle, Oval
+
+rect1 = Rectangle()
+
+rect1.set_width(200)
+rect1.set_height(100)
+rect1.set_color("blue")
+rect1.draw()
+
+
+rec2 = Rectangle()
+
+rec2.set_width(50)
+rec2.set_height(150)
+rec2.set_color("yellow")
+rec2.draw()
+rec2.set_x(100)
+rec2.set_y(100)
+
+ov1 = Oval()
+
+ov1.randomize(20)
+ov1.draw()
+
+tri1 = Triangle(5,5, 100,5, 100,200)
+tri1.draw()

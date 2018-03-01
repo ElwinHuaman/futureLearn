92cb1ae8b799678f14c3fac67ea2a3fc1804c6ff
Elwin Huaman <elwinlhq@hotmail.com>
2018-03-01 16:20:19 +0100

    Day2

---
+++ item.py
@@ -0,0 +1,18 @@
+class Item():
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+    
+    def set_name(self, item_name):
+        self.name = item_name
+    def get_name(self):
+        return self.name
+    
+    def set_description(self, item_description):
+        self.description = item_description
+    def get_description(self):
+        return self.description
+    
+    def get_details(self):
+        print("Name: "+self.name)
+        print("Description: "+ self.description)
--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 from room import Room
+from item import Item
 
 kitchen = Room("Kitchen")
 kitchen.set_description("A dark and dirty room buzzing with flies.")
-kitchen.get_description()
-kitchen.describe()
 
-dining_hall = Room("DInning Hall")
-dining_hall.set_description("A large room with ornate golden decorations on each wall.")
+dinning_hall = Room("DInning Hall")
+dinning_hall.set_description("A large room with ornate golden decorations on each wall.")
 
-kitchen.linked_rooms(dining_hall,"south")
+ballroom = Room("Ballroom")
+ballroom.set_description("A vast room with a shiny wc")
+
+kitchen.link_room(dinning_hall, "south") 
+dinning_hall.link_room(kitchen, "north") 
+dinning_hall.link_room(ballroom, "west") 
+ballroom.link_room(dinning_hall, "east")
+
+#dining_hall.get_details()
+# current_room = kitchen          
+
+# while True:		
+#     print("\n")         
+#     current_room.get_details()         
+#     command = input("> ")    
+#     current_room = current_room.move(command)
+
+# sword = Item("Sword","Small and easy to use in a battle...")
+# sword.get_details()
